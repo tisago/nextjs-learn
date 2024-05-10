e95fb06976da065ea4902de63bceca50ef6675e5
santiago <tisagrid@gmail.com>
2024-05-10 19:40:13 -0300

    DB

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+import { inter } from "./ui/fonts";
+import "./ui/global.css"
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} antialiased`}>
+        {children}
+      </body>
+    </html>
+  );
+}
